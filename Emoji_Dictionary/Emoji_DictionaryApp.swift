b510ba586fd93e5b3f18664255529210b326b9e0
Lam Nguyen <lamcnguyen89@gmail.com>
2022-01-07 17:27:02 -0500

    Initial Commit

---
+++ Emoji_Dictionary/Emoji_DictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_DictionaryApp.swift
+//  Emoji_Dictionary
+//
+//  Created by Lam Nguyen on 1/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Emoji_DictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
